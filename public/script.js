b514faaba6c927cb317f4d73a6f4d888f456d766
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-17 20:10:57 +0700

    add loading

--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+window.addEventListener('load', () => {
+  let coll = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      let content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+
+  setTimeout(() => {
+    document.getElementById('loading').style.display = 'none';
+  }, 3000);
+
+})
