ad1630cbe0289d9b7bb5c125edee56699fed2a54
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-19 10:18:31 +0700

    update

--- public/script.js
+++ public/script.js
@@ -2,6 +2,18 @@
 
 window.addEventListener('load', () => {
 
+  let certificateBtn = document.getElementsByClassName('certificate-btn');
+  let certificateModal = document.getElementsByClassName('certificate-modal');
+  let certificateModalClose = document.getElementsByClassName('certificate-modal-close');
+  for (let i = 0; i < certificateBtn.length; i++) {
+    certificateBtn[i].addEventListener('click', () => {
+      certificateModal[i].style.display = 'block';
+    })
+    certificateModalClose[i].addEventListener('click', () => {
+      certificateModal[i].style.display = 'none'
+    })
+  }
+
   setTimeout(() => {
     document.getElementById('loading').style.height = 0;
   }, 1000);
